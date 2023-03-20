df033f5dea22885258e5bd280489f8a6ac1e7035
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-20 13:10:32 +0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+it("should render App component without crashing", ()=>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const element = screen.getByText("Modern Testing");
+  expect(element).toBeInTheDocument();
+}
+)
+
+
+
+
